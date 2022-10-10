e4fedc30ff8f809a74231d40cfbb11380010632d
NetoMarcal <neto.marcal.dev@gmail.com>
2022-10-10 11:10:27 -0300

    Remoção desnecessária da div

--- src/components/Modal/Customer/change/index.js
+++ src/components/Modal/Customer/change/index.js
@@ -5,24 +5,22 @@ import RegisterCostumer from '../../../Customer/Register/index';
 const ModalConfirm = ({action, setAction, modal, toggle, customer, setCustomerUpdate, document, setTestDocumentExist}) => {
 
   return (
-    <div>     
-      <Modal isOpen={modal.modal} toggle={toggle} className='modal-dialog modal-xl'>
-        { action === 'change' ? <ModalHeader toggle={toggle}> <h4 className='title-page'>Alterar Cliente</h4></ModalHeader> : '' }
-        <ModalBody>
-            <RegisterCostumer 
-                customer={customer} 
-                action={action}
-                setAction={setAction}
-                setCustomerUpdate={setCustomerUpdate}
-                document={document}
-                setTestDocumentExist={setTestDocumentExist}
-            /> 
-        </ModalBody>
-        <ModalFooter >
-          <Button id='acao1' className='btn-block btn-light' color="secondary"  onClick={toggle}>C A N C E L A R</Button>{' '}
-        </ModalFooter>
-      </Modal>
-    </div>
+    <Modal isOpen={modal.modal} toggle={toggle} className='modal-dialog modal-xl'>
+      { action === 'change' ? <ModalHeader toggle={toggle}> <h4 className='title-page'>Alterar Cliente</h4></ModalHeader> : '' }
+      <ModalBody>
+          <RegisterCostumer 
+              customer={customer} 
+              action={action}
+              setAction={setAction}
+              setCustomerUpdate={setCustomerUpdate}
+              document={document}
+              setTestDocumentExist={setTestDocumentExist}
+          /> 
+      </ModalBody>
+      <ModalFooter >
+        <Button id='acao1' className='btn-block btn-light' color="secondary"  onClick={toggle}>C A N C E L A R</Button>{' '}
+      </ModalFooter>
+    </Modal>
   );
 }
 
